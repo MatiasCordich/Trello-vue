6df4639c426d1437458c9fa554b767fc3f015296
MatiasCordich <m.scordich@gmail.com>
2024-04-02 23:15:28 -0300

    Fixed parse ObjectID mongo and finished services tasks

--- backend/services/task.js
+++ backend/services/task.js
@@ -1,5 +1,4 @@
 const TaskModel = require('../models/Task')
-const { ObjectId } = require('mongoose').Types;
 
 // Obtener todas las tareas
 const getAllTasks = async () => {
@@ -20,8 +19,18 @@ const insertTask = async (task) => {
   const response = await TaskModel.create(task)
   return response
 }
+
 // Editar tarea
+const updateTask = async (data, id) => {
+  const response = await TaskModel.findByIdAndUpdate({_id: id}, data, {new: true})
+
+  return response
+}
 // Eliminar tarea
+const deleteTask = async (id) => {
+  const response = await TaskModel.findByIdAndDelete({_id: id})
 
+  return response
+}
 
-module.exports = {getAllTasks, getOneTask, insertTask}
+module.exports = {getAllTasks, getOneTask, insertTask, updateTask, deleteTask}
